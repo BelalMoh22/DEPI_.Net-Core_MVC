9e5f0aca919ef6e1500574f7c4e08eb17100f012
Belal Mohamed <ibelalmohammed@gmail.com>
2025-10-13 22:26:21 +0300

    Add project files.

---
+++ Day22MVCDemoLab/Controllers/StudentsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Day22MVCDemoLab.Models;
+namespace Day22MVCDemoLab.Controllers
+{
+    public class StudentsController : Controller
+    {
+
+       public IActionResult Index()
+        {
+            var result = Student.students.ToList();
+            return View(result);
+        }
+
+        // students/ShowMsgWithPara/100
+        public IActionResult ShowMsgWithPara(int id)
+        {
+            var result = new ContentResult();
+            result.Content = $"Welcome Message with id {id}";
+            return result;
+        }
+
+        // students/ShowMsgWithName?name=belal
+        public IActionResult ShowMsgWithName(string Name)
+        {
+            var result = new ContentResult();
+            result.Content = $"Welcome Message with Name {Name}";
+            return result;
+        }
+
+        // students/BackToIndex
+        public IActionResult BackToIndex()
+        {
+            return RedirectToAction("Index");
+        }
+
+        // students/BackToIndexHome
+        public IActionResult BackToIndexHome()
+        {
+            return RedirectToAction("Index" , controllerName: "Home");
+        }
+
+        //students/showGoogle
+        public IActionResult showGoogle()
+        {
+            return Redirect("http://www.google.com");
+        }
+
+        //students/showYahoo
+        public IActionResult showYahoo()
+        {
+            return RedirectPermanent("http://www.Yahoo.com");
+        }
+
+        // 1.Content "String"    => ContentResult
+        // Students/SayContent
+        public IActionResult SayContent() //ContentResult
+        {
+            ContentResult result = new ContentResult();
+            result.Content= "Hello World From MVC......";
+            return result;
+        }
+
+        // 2. View"HTML"        => ViewResult
+        public IActionResult ShowView() //  ViewResult
+        {
+            ViewResult result = new ViewResult();
+            result.ViewName = "~/Views/Students/MyView.cshtml";
+            return result;
+        }
+
+        //3- Json               => JsonResult
+        public IActionResult ShowJson()
+        {
+            return Json(new { ID = 1, Name = "Osama", Salary = 20000 });
+        }
+         
+        //4- File               => FileResult
+        public IActionResult ShowFile()
+        {
+            return File("~/Smart Welcome in MVC.txt", "text/plain");
+        }
+
+        //5- Empty              => Emptyresult
+        public IActionResult ShowEmpty()
+        {
+            return new EmptyResult(); //or return null;
+        }
+    }
+}
---
+++ Day22MVCDemoLab/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace Day22MVCDemoLab.Models
+{
+    public class Student
+    {
+
+        public static List<Student> students = new List<Student>()
+        {
+            new Student() {Id=1 , Name = "Tamer1" , Age = 20},
+            new Student() {Id=2 , Name = "Tamer2" , Age = 30},
+            new Student() {Id=3 , Name = "Tamer3" , Age = 40},
+            new Student() {Id=4 , Name = "Tamer4" , Age = 50},
+            new Student() {Id=5 , Name = "Tamer5" , Age = 25}
+        };
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
